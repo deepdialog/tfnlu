078b1bd7850046188a18ba3474f5e88b271b2b20
deepdialog <thebotbot@sina.com>
2020-04-14 14:51:10 +0800

    update

--- tfnlu/__init__.py
+++ tfnlu/__init__.py
@@ -1,4 +1,5 @@
 from .parser.parser import Parser
 from .tagger.tagger import Tagger
+from .utils import TempDir, serialize_dir, deserialize_dir
 
-__all__ = ['Parser', 'Tagger']
+__all__ = ['Parser', 'Tagger', 'TempDir', 'serialize_dir', 'deserialize_dir']
---
+++ tfnlu/utils/__init__.py
@@ -0,0 +1,4 @@
+from .temp_dir import TempDir
+from .serialize_dir import serialize_dir, deserialize_dir
+
+__all__ = ['TempDir', 'serialize_dir', 'deserialize_dir']
