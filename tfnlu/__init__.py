c01c972aa1f8ee21882dc02e3c400cc519e740e5
qhduan <mail@qhduan.com>
2021-07-08 04:39:15 +0800

    try fix github ci; add extract entities in top

--- tfnlu/__init__.py
+++ tfnlu/__init__.py
@@ -3,6 +3,7 @@ from .parser.parser import Parser
 from .tagger.tagger import Tagger
 from .classification.classification import Classification
 from .utils import serialize_dir, deserialize_dir, Transformer
+from .tagger.extract_entities import extract_entities
 
 
 VERSION_FILE = os.path.join(os.path.dirname(__file__), 'version.txt')
@@ -11,6 +12,6 @@ __version__ = open(VERSION_FILE).read().strip()
 
 __all__ = [
     'Parser', 'Tagger', 'Classification', 'TempDir', 'serialize_dir',
-    'deserialize_dir', 'Transformer',
+    'deserialize_dir', 'Transformer', 'extract_entities',
     '__version__'
 ]
